03466edd2582ea7d77ff49628d19b4d1dfb2f60c
prasoondeva <prasoondeva@gmail.com>
2018-12-30 15:30:14 -0700

    final commit

--- Inbox/test/inbox.test.js
+++ Inbox/test/inbox.test.js
@@ -1,9 +1,11 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); //Web3 start with capital letter because it is considered as Class and can be used to make instance of web3
-const {interface, bytecode} = require('../compile');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const {interface, bytecode} = require('../compile');
 
 let accounts;
 let inbox;
@@ -17,10 +19,23 @@ beforeEach(async () => {
     inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({data : bytecode, arguments : ['Hi There!']})
     .send({from : accounts[0], gas : '1000000'});
+
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
     it('deploys a contract',() => {
         assert.ok(inbox.options.address);
+    });
+
+    it('has a defualt messsage', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi There!');
+    });
+
+    it('can set new message', async () => {
+        await inbox.methods.SetMessage('Bye').send({ from : accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Bye');
     });
 });
